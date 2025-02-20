5215ec3cbea2d00eb48f1fdfe6a65b488ba850af
silaholmes <alaalolo2030aa@gmail.com>
2025-02-20 15:14:55 +0200

    Update user_data.py

--- user_data.py
+++ user_data.py
@@ -8,6 +8,8 @@ genders = ["Male", "Female"]
 allergies = ["Peanuts", "Wheat", "Shellfish", "Lactose", "None"]
 diabetes_types = ["Type 1", "Type 2", "None"]
 #blood_sugar_ranges = ["70-120", "80-130", "85-135", "90-140"]
+diets = ["Keto","Vegan","Low-Carb","High-Carb","None"]
+
 min_sugar = random.randint(70, 90)
 max_sugar = random.randint(min_sugar + 10, 180)
 
@@ -18,16 +20,17 @@ for _ in range(1000):
     height = random.randint(150, 200)
     weight = random.randint(45, 120)
     allergy = random.choice(allergies)
+    diet = random.choice(diets)
     diabetes = random.choice(diabetes_types)
     insulin_coefficient = f"{1}-{random.randint(5, 20)}" if diabetes == "Type 1" else None
     blood_sugar_range = f"{min_sugar}-{max_sugar}" if diabetes != "None" else None
     
     data.append([
-        gender, dob, height, weight, allergy, diabetes, insulin_coefficient, blood_sugar_range
+        gender, dob, height, weight, allergy, diet, diabetes, insulin_coefficient, blood_sugar_range
     ])
 
 df = pd.DataFrame(data, columns=[
-    "Gender", "Date of Birth", "Height (cm)", "Weight (kg)", "Allergy", 
+    "Gender", "Date of Birth", "Height (cm)", "Weight (kg)", "Allergy", "Diet",
     "Diabetes", "Insulin Coefficient", "Preferred Blood Sugar Range"
 ])
 
